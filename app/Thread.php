187bfd5f7ce1d07af06d78dbe2ae2b14746cdb6f
navinweb <anatoliykirzo@gmail.com>
2018-09-30 15:14:09 +0300

    --date=1 day ago
    add channel model adn factory

--- app/Thread.php
+++ app/Thread.php
@@ -23,6 +23,10 @@ class Thread extends Model
 		return $this->belongsTo( User::class, 'user_id' );
 	}
 
+	public function channel(  ) {
+		return $this->belongsTo(Channel::class);
+	}
+
 	public function addReply( $reply )
 	{
 		$this->replies()->create( $reply );
