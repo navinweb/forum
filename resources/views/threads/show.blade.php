8b78855b8ee38fe76596698ee450a38f857a2f00
navinweb <anatoliykirzo@gmail.com>
2018-10-09 13:24:37 +0300

    add meta sidebar

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container">
-        <div class="row justify-content-center">
+        <div class="row">
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header">{{ $thread->title }}</div>
@@ -14,18 +14,27 @@
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div class="row justify-content-center">
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <article>
+                            <p class="body">This thread was published {{ $thread->created_at->diffForHumans() }} by <a
+                                        href="#">{{ $thread->creator->name }}</a> and currently
+                                has {{ $thread->replies_count }} {{ str_plural('comment', $thread->replies_count ) }}
+                            </p>
+                        </article>
+                    </div>
+                </div>
+            </div>
+
             <div class="col-md-8">
                 @foreach($thread->replies as $reply)
                     @include('threads.reply')
                 @endforeach
             </div>
-        </div>
 
-        @if(auth()->check())
-            <div class="row justify-content-center">
+            @if(auth()->check())
                 <div class="col-md-8">
                     <form action="{{ $thread->path() . '/replies' }}" method="post">
                         {{ csrf_field() }}
@@ -35,9 +44,11 @@
                         <button type="submit" class="btn btn-default">Post</button>
                     </form>
                 </div>
-            </div>
-        @else
-            <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate in this discussion.</p>
-        @endif
+            @else
+                <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate in this
+                    discussion.</p>
+            @endif
+
+        </div>
     </div>
 @endsection
