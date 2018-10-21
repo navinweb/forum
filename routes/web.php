d3e9414b9c8855d16af11cbf1503b4ee37a01a9e
navinweb <anatoliykirzo@gmail.com>
2018-10-22 02:26:14 +0300

    add profile test

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,5 @@ Route::post('/threads/{channel}/{thread}/replies', 'RepliesController@store');
 
 // Favorites
 Route::post('/replies/{reply}/favorites', 'FavoritesController@store');
+
+Route::get('/profiles/{user}', 'ProfilesController@show');
