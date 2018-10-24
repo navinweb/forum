49ac8e1a29eeae0858d9f87cb567271b4aa2a136
navinweb <anatoliykirzo@gmail.com>
2018-10-25 02:37:53 +0300

    add profile page

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -2,6 +2,23 @@
 
 @section('content')
     <div class="container">
-        {{ $profileUser->name }}
+        <div class="page-header">
+            <h1>
+                {{ $profileUser->name }}
+                <small>Since {{ $profileUser->created_at->diffForHumans() }}</small>
+            </h1>
+        </div>
+
+        @foreach($profileUser->threads as $thread)
+            <div class="card">
+                <div class="card-header">{{ $thread->title }}</div>
+                <div class="card-body">
+                    <article>
+                        <a href="{{$thread->creator->id}}">{{ $thread->creator->name }}</a> posted:
+                        <div class="body">{{ $thread->body }}</div>
+                    </article>
+                </div>
+            </div>
+        @endforeach
     </div>
 @endsection
