1c118dcd3d97254b34046f045e94b73fa163ac6d
navinweb <anatoliykirzo@gmail.com>
2018-10-30 03:39:42 +0300

    add test for guets deleting thread

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -61,6 +61,17 @@ class CreateThreadsTest extends TestCase
 	}
 
 	/** @test */
+	public function a_thread_cant_be_deleted_by_guest()
+	{
+		$this->withExceptionHandling();
+		$thread = create( 'App\Thread' );
+
+		$response = $this->delete( $thread->path() );
+
+		$response->assertRedirect( '/login' );
+	}
+
+	/** @test */
 	public function a_thread_can_be_deleted()
 	{
 		$this->signIn();
