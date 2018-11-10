bd7836eedce1e1945077a7844e3ed30e86097e29
navinweb <anatoliykirzo@gmail.com>
2018-11-10 20:34:17 +0300

    separate activity by date

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -10,7 +10,9 @@ class ProfilesController extends Controller
 	public function show( User $user )
 	{
 
-		$activities = $user->activity()->latest()->with( 'subject' )->get();
+		$activities = $user->activity()->latest()->with( 'subject' )->get()->groupBy( function ( $activity ) {
+			return $activity->created_at->format( 'Y-m-d' );
+		} );
 
 		return view( 'profiles.show', [
 			'profileUser' => $user,
