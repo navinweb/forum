0a7682c15595c5a148500e06030280f3220698b5
navinweb <anatoliykirzo@gmail.com>
2018-11-13 21:25:58 +0300

    improve feed test

--- tests/Unit/ActivityTest.php
+++ tests/Unit/ActivityTest.php
@@ -46,15 +46,20 @@ class ActivityTest extends TestCase
 	{
 		$this->signIn();
 
-		create( 'App\Thread', [ 'user_id' => auth()->id() ] );
-		create( 'App\Thread', [
-			'user_id'    => auth()->id(),
+		create( 'App\Thread', [ 'user_id' => auth()->id() ], 2 );
+
+		auth()->user()->activity()->first()->update( [
 			'created_at' => Carbon::now()->subWeek()
 		] );
 
-		$feed = Activity::feed(auth()->user());
-		$this->assertTrue($feed->keys()->contains(
-			Carbon::now()->format('Y-m-d')
-		));
+		$feed = Activity::feed( auth()->user() );
+
+		$this->assertTrue( $feed->keys()->contains(
+			Carbon::now()->format( 'Y-m-d' )
+		) );
+
+		$this->assertTrue( $feed->keys()->contains(
+			Carbon::now()->subWeek()->format( 'Y-m-d' )
+		) );
 	}
 }
