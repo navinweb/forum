85598a839c352a3c86e91d15101916950b44aba4
navinweb <anatoliykirzo@gmail.com>
2018-12-12 16:47:41 +0300

    add replyCount increase when add

--- public/js/app.js
+++ public/js/app.js
@@ -47973,6 +47973,8 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 	methods: {
 		add: function add(reply) {
 			this.items.push(reply);
+
+			this.$emit('added');
 		},
 		remove: function remove(index) {
 			this.items.splice(index, 1);
--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -31,7 +31,7 @@
                         </div>
                     </div>
 
-                    <replies :data="{{ $thread->replies }}" @removed="repliesCount--"></replies>
+                    <replies :data="{{ $thread->replies }}" @added="repliesCount++" @removed="repliesCount--"></replies>
 
                     {{--{{ $replies->links() }}--}}
 
