99179a5462819d614369fcf3ce21ccd90f728df3
navinweb <anatoliykirzo@gmail.com>
2019-01-02 16:36:16 +0300

    little refac

---
+++ app/Events/ThreadHasNewReply.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Events;
+
+use App\Reply;
+use App\Thread;
+use Illuminate\Queue\SerializesModels;
+
+class ThreadHasNewReply
+{
+	use SerializesModels;
+	public $thread;
+	public $reply;
+
+	/**
+	 * Create a new event instance.
+	 *
+	 * @param Thread $thread
+	 * @param Reply $reply
+	 */
+	public function __construct( $thread, $reply )
+	{
+		//
+		$this->thread = $thread;
+		$this->reply  = $reply;
+	}
+}
