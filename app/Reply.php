309053bad58f32fafd6f997b8f0a775de6a060e5
navinweb <anatoliykirzo@gmail.com>
2019-01-12 21:25:36 +0300

    add link for mentioned users

--- app/Reply.php
+++ app/Reply.php
@@ -13,19 +13,19 @@ class Reply extends Model
 
 	protected $with = [ 'owner', 'favorites' ];
 
-	protected $appends = ['favoritesCount', 'isFavorite'];
+	protected $appends = [ 'favoritesCount', 'isFavorite' ];
 
 	protected static function boot()
 	{
 		parent::boot();
 
-		static::created(function($reply){
-			$reply->thread->increment('replies_count');
-		});
+		static::created( function ( $reply ) {
+			$reply->thread->increment( 'replies_count' );
+		} );
 
-		static::deleted(function($reply){
-			$reply->thread->decrement('replies_count');
-		});
+		static::deleted( function ( $reply ) {
+			$reply->thread->decrement( 'replies_count' );
+		} );
 	}
 
 	public function owner()
@@ -43,16 +43,20 @@ class Reply extends Model
 		return $this->thread->path() . "#reply-{$this->id}";
 	}
 
-	public function wasJustPublished(  )
+	public function wasJustPublished()
 	{
-		return $this->created_at->gt(Carbon::now()->subMinute());
+		return $this->created_at->gt( Carbon::now()->subMinute() );
 	}
 
 	public function mentionedUsers()
 	{
-		preg_match_all( '/\@([^\s\.]+)/', $this->body, $matches );
+		preg_match_all( '/@([\w\-]+)/', $this->body, $matches );
 
 		return $matches[1];
+	}
 
+	public function setBodyAttribute( $body )
+	{
+		$this->attributes['body'] = preg_replace( '/@([\w\-]+)/', '<a href="/profiles/$1">$0</a>', $body );
 	}
 }
