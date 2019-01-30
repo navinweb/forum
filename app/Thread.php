5b00b9ecb8cbba754c1efd5f45f7fa848b4bae4e
navinweb <anatoliykirzo@gmail.com>
2019-01-30 20:45:41 +0300

    add post search

--- app/Thread.php
+++ app/Thread.php
@@ -6,10 +6,11 @@ use App\Events\ThreadHasNewReply;
 use App\Events\ThreadReceivedNewReply;
 use App\Notifications\ThreadWasUpdated;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Thread extends Model
 {
-	use RecordsActivity;
+	use RecordsActivity, Searchable;
 
 	protected $guarded = [];
 
